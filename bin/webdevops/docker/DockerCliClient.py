a1b4d24817b31582b8d197ca769649564bff74df
Teddy Erembert Dubal <teddy@sunsms.fr>
2020-10-27 16:31:43 -0400

    *

--- bin/webdevops/docker/DockerCliClient.py
+++ bin/webdevops/docker/DockerCliClient.py
@@ -41,19 +41,20 @@ class DockerCliClient(DockerBaseClient):
         Build dockerfile
         """
         
-        # instanceName = name.replace('/','-').replace(':','-')
+        instanceName = name.replace('/','-').replace(':','-')
         
-        # cmdStop = ['docker','stop',instanceName]
-        # Command.execute(cmdStop)
+        print instanceName + " **************************************"
+        cmdStop = ['docker','stop',instanceName]
+        Command.execute(cmdStop)
 
-        # cmdRm = ['docker','rm',instanceName]
-        # Command.execute(cmdRm)
+        cmdRm = ['docker','rm',instanceName]
+        Command.execute(cmdRm)
         
-        # cmdBuilderInstance = ['docker','buildx', 'create','--name',instanceName]
-        # Command.execute(cmdBuilderInstance)
+        cmdBuilderInstance = ['docker','buildx', 'create','--name',instanceName]
+        Command.execute(cmdBuilderInstance)
 
-        # cmdBuilderInstanceUse = ['docker','buildx', 'use',instanceName]
-        # Command.execute(cmdBuilderInstanceUse)
+        cmdBuilderInstanceUse = ['docker','buildx', 'use',instanceName]
+        Command.execute(cmdBuilderInstanceUse)
         
         cmd = ['docker','buildx', 'build','--platform', 'linux/arm64,linux/arm/v7','--tag', name, os.path.dirname(path)]
 
